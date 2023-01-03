22ee9ce0ebfe9683ddf1ef13cd6836a09f3814c8
Haxrox <56497846+Haxrox@users.noreply.github.com>
2023-01-02 20:30:39 -0800

    video replicates text changes to the canvas as well

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # CanvasVideoPlayer
 HTML Canvas video player
+
+## TODO
+[x] Display a video inside the canvas
+[x] Create a new video element that plays from whatever is in the canvas (for PIP)
+[ ] Add play, seek, PIP, caption buttons to the canvas
+[ ] Add button "listeners" to the buttons in the canvas -> make sure this translates nicely to PIP
+[ ] Add subtitles to Canvas
--- main.js
+++ main.js
@@ -5,22 +5,34 @@ window.addEventListener("load", () => {
     const haxVideo = document.createElement("video");
     const videoCard = document.getElementById("haxVideoCard");
     videoCard.appendChild(haxVideo);
+    video.crossOrigin = 'anonymous';
 
     const stream = canvas.captureStream();
     haxVideo.srcObject = stream;
     haxVideo.muted = true;
     haxVideo.controls = true;
+
+    canvasContext.textAlign = 'left';
+    canvasContext.textBaseline = 'top';
+    canvasContext.font = '20px sans-serif';
+    canvasContext.fillStyle = 'white';
+    canvasContext.fillText(Date.now().toString(), 10, 10);
         
     video.addEventListener("play", async () => {
         canvas.width = video.clientWidth;
         canvas.height = video.clientHeight;
 
         canvasContext.drawImage(video, 0, 0, video.clientWidth, video.clientHeight);
-        canvas.toDataURL();
 
         let interval = setInterval(() => {
             if (!video.paused && !video.ended) {
                 canvasContext.drawImage(video, 0, 0, video.clientWidth, video.clientHeight);
+                canvasContext.restore();
+                canvasContext.textAlign = 'left';
+                canvasContext.textBaseline = 'top';
+                canvasContext.font = '20px sans-serif';
+                canvasContext.fillStyle = 'white';
+                canvasContext.fillText(Date.now().toString(), 10, 10);
             } else {
                 clearInterval(interval);
             }
